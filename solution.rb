fccfa69830837c3994b45c2509b0273c73e3746d
Brendan Winter <brendan@leadadvo.com>
2013-05-16 23:43:53 -0700

    Modified solution a little, it's more confusing to read but lines of code are shorter - just to see if anything simpler came out of it. I also modified the test output quite a bit. Eventaully I would like to make this more complex.

--- solution.rb
+++ solution.rb
@@ -6,48 +6,53 @@
 #This algoritm solves the problem in linear time. (For some odd resion, the couple places I looked online had nlongn soltions. So, hopefully I haven't missed something.)
 
 def longestSequence (numbers)
-  tempFirst = 0;
-  tempLast = 0;
-  tempLength = 1;
-  longestFirst = nil;
-  longestLast = nil;
-  longest = 1;
-  for i in (0..numbers.length)
-    if numbers[i + 1] != nil
+  #If numbers.length > 0 then there is a subsequence of at least length 1.
+  #This is important because my algorithm looks at the next element
+  longest      = tempLength = 1;
+  longestFirst = tempFirst  = 0;
+  longestLast  = tempLast   = 0;
+  
+  while tempLast < numbers.length - 1 do
+    tempLength = tempLength + 1;
 
-      #If integers can differ by more than 1
-      #if numbers[i] < numbers[i + 1]
-      if numbers[i + 1] - numbers[i] == 1
-        tempLast = i + 1;
-        tempLength += 1;
+    #If sequence elements can differ by more than 1 (e.g. [3,6,8] is a valid subsequence)
+    #if numbers[tempLast] + 1 < numbers[tempLast + 1]
 
-        if tempLength > 1 and tempLength > longest
-          longestFirst = tempFirst;
-          longestLast = tempLast;
-          longest = tempLength;
-        end
+    #If the two elements are out of order restart the current sequence.
+    if numbers[tempLast] + 1 != numbers[tempLast + 1]
+      tempFirst = tempLast + 1;
+      tempLength = 1;
+    end
 
-      else
-        tempFirst = i + 1;
-        tempLast = i + 1;
-        tempLength = 1;
-      end
-
+    tempLast += 1;
+    if tempLength > longest
+      longestFirst = tempFirst;
+      longestLast = tempLast;
+      longest = tempLength;
     end
   end
 
+  #If the longest found subsequence is two or larger, slice it out of the array and return it.
   if longest > 1
-    numbers.slice(longestFirst, longestLast - longestFirst + 1);
-  else
+    return numbers.slice(longestFirst, longestLast - longestFirst + 1);
+  else #Else return an empty array.
     return [];
   end
 end
 
 def tests()
-  def green; 2; end
-  def red; 1; end 
-  def c( fg, bg = nil ); "#{fg ? "\x1b[38;5;#{fg}m" : ''}#{bg ? "\x1b[48;5;#{bg}m" : ''}" end
-  def ec; "\x1b[0m"; end
+  def color(fg, bg = nil)
+    return "#{fg ? "\x1b[38;5;#{fg}m" : ''}#{bg ? "\x1b[48;5;#{bg}m" : ''}";
+  end
+  def ec()
+    return "\x1b[0m";
+  end
+  def green(str)
+    return color(2) + str + ec();
+  end
+  def red(str)
+    return color(1) + str + ec();
+  end 
 
   examples = 
   {
@@ -68,11 +73,18 @@ def tests()
     [1,2,3,2,3,4,5] => [2,3,4,5],
     [1,1,2,3,0,4,3,3,4,5] => [1,2,3],
   };
-  puts "#{'Expected'.rjust(14, ' ')}    Result";
-  for example, expected in examples
-    puts "#{expected.to_s().rjust(14,' ')} #{((longestSequence(example) == expected) ? c(green) + '==' : c(red) + "!=") + ec()} #{longestSequence(example)}";
+  longestInput = examples.keys().collect{|key| key.to_s().size}.max;
+  puts longestInput
+  puts "+ #{    '-'.ljust(longestInput, '-')} + #{       '-'.rjust(longestInput, '-')}--#{       '-'.ljust(longestInput, '-')} +";
+  puts "| #{'Input'.ljust(longestInput, ' ')} | #{'Expected'.rjust(longestInput, ' ')}  #{'Returned'.ljust(longestInput, ' ')} |";
+  puts "+ #{    '-'.ljust(longestInput, '-')} + #{       '-'.rjust(longestInput, '-')}--#{       '-'.ljust(longestInput, '-')} +";
+  for input, correct in examples
+    returned = longestSequence(input);
+
+    puts "| " + input.to_s().ljust(longestInput, ' ') + " | " + correct.to_s().rjust(longestInput,' ') + (returned == correct ? green('==') : red('!=')) + returned.to_s().ljust(longestInput, ' ') + " |";
   end
+  puts "+ #{    '-'.ljust(longestInput, '-')} + #{       '-'.rjust(longestInput, '-')}--#{       '-'.ljust(longestInput, '-')} +";
 end
 
-
+tests();
 
